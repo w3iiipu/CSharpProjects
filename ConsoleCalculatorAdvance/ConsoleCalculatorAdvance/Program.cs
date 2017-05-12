b5a7c2ed716e0601e4e8d4717a2d9023b0138cf0
w3iiipu <w3iiipu@gmail.com>
2017-05-12 11:43:27 -0400

    reorganize

---
+++ ConsoleCalculatorAdvance/ConsoleCalculatorAdvance/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool keepAsking = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Please Enter an equation.");
+                    string input = Console.ReadLine();
+
+                    double answer = EvaluateOrderOfOperations(input);
+                    Console.WriteLine(answer);
+                    Console.WriteLine("");
+                    Console.WriteLine("Again?");
+                    string again = Console.ReadLine();
+                    if (again != "y")
+                    {
+                        keepAsking = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (keepAsking);
+
+
+
+
+        }
+
+        public static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+
+        public static double EvaluateOrderOfOperations(string input)
+        {
+            double answer = 0;
+            var foundIndexesADD = new List<int>();
+            var foundIndexesSUB = new List<int>();
+            var foundIndexesMUL = new List<int>();
+            var foundIndexesDIV = new List<int>();
+            var allOperands = new List<int>();
+
+            if (input.Contains("+"))
+            {
+                for (int i = input.IndexOf('+'); i > -1; i = input.IndexOf('+', i + 1))
+                {
+                    foundIndexesADD.Add(i);
+                    allOperands.Add(i);
+                }
+            }
+
+            if (input.Contains("-"))
+            {
+                for (int i = input.IndexOf('-'); i > -1; i = input.IndexOf('-', i + 1))
+                {
+                    foundIndexesSUB.Add(i);
+                    allOperands.Add(i);
+                }
+            }
+
+            if (input.Contains("*"))
+            {
+                for (int i = input.IndexOf('*'); i > -1; i = input.IndexOf('*', i + 1))
+                {
+                    foundIndexesMUL.Add(i);
+                    allOperands.Add(i);
+                }
+            }
+
+            if (input.Contains("/"))
+            {
+                for (int i = input.IndexOf('/'); i > -1; i = input.IndexOf('/', i + 1))
+                {
+                    foundIndexesDIV.Add(i);
+                    allOperands.Add(i);
+                }
+            }
+
+            allOperands.Sort();
+            int y = 0;
+            string[] parts;
+            int index = 1;
+            int lastIndex = 0;
+            int operandIndex = 0;
+            var connectingOperators = new List<string>();
+
+            if (IsOdd(allOperands.Count))
+            {
+                parts = new string[(allOperands.Count + 1) / 2];
+                for (var x = 0; x <= allOperands.Count; x++)
+                {
+                    if (index != allOperands.Count + 1)
+                    {
+                        operandIndex = allOperands[x];
+                        if (!IsOdd(index))
+                        {
+                            parts[y] = input.Substring(lastIndex, operandIndex - lastIndex);
+                            lastIndex = operandIndex + 1;
+                            connectingOperators.Add(input.Substring(operandIndex, 1));
+                            y++;
+                        }
+
+                        index++;
+                    }
+                    else
+                    {
+                        operandIndex = input.Length;
+                        if (!IsOdd(index))
+                        {
+                            parts[y] = input.Substring(lastIndex, operandIndex - lastIndex);
+                            lastIndex = operandIndex + 1;
+
+                            y++;
+                        }
+
+                        index++;
+                    }
+                }
+            }
+            else
+            {
+                parts = new string[((allOperands.Count + 1) / 2) + 1];
+                for (var x = 0; x <= allOperands.Count; x++)
+                {
+
+
+                    if (index != allOperands.Count + 1)
+                    {
+                        operandIndex = allOperands[x];
+                        if (!IsOdd(index))
+                        {
+                            parts[y] = input.Substring(lastIndex, operandIndex - lastIndex);
+                            lastIndex = operandIndex + 1;
+                            connectingOperators.Add(input.Substring(operandIndex, 1));
+                            y++;
+                        }
+
+                        index++;
+                    }
+                    else
+                    {
+                        operandIndex = input.Length;
+                        if (IsOdd(index))
+                        {
+                            parts[y] = input.Substring(lastIndex, operandIndex - lastIndex);
+                        }
+
+                        index++;
+                    }
+                }
+            }
+
+
+            foreach (string part in parts)
+            {
+                if (part.Contains("+"))
+                {
+                    string[] split = part.Split('+');
+                    answer = answer + Add(Convert.ToDouble(split[0]), Convert.ToDouble(split[1]));
+                }
+
+                if (part.Contains("-"))
+                {
+                    string[] split = part.Split('-');
+                    answer = answer + Subt(Convert.ToDouble(split[0]), Convert.ToDouble(split[1]));
+                }
+
+                if (part.Contains("*"))
+                {
+                    string[] split = part.Split('*');
+                    answer = answer + Mult(Convert.ToDouble(split[0]), Convert.ToDouble(split[1]));
+                }
+
+                if (part.Contains("/"))
+                {
+                    string[] split = part.Split('/');
+                    answer = answer + Divide(Convert.ToDouble(split[0]), Convert.ToDouble(split[1]));
+                }
+
+                if (!part.Contains("+") && !part.Contains("-") && !part.Contains("*") && !part.Contains("/"))
+                {
+                    switch (connectingOperators[0])
+                    {
+                        case "+":
+                            answer = answer + Convert.ToDouble(part);
+                            break;
+                        case "-":
+                            answer = answer - Convert.ToDouble(part);
+                            break;
+                        case "*":
+                            answer = answer * Convert.ToDouble(part);
+                            break;
+                        case "/":
+                            answer = answer / Convert.ToDouble(part);
+                            break;
+                    }
+
+                }
+
+            }
+
+
+            return answer;
+        }
+
+
+
+        public static double Add(double a, double b)
+        {
+            double x = 0f;
+            x = a + b;
+            return x;
+        }
+        public static double Subt(double a, double b)
+        {
+            double x = 0f;
+            x = a - b;
+            return x;
+        }
+        public static double Mult(double a, double b)
+        {
+            double x = 0f;
+            x = a * b;
+            return x;
+        }
+        public static double Divide(double a, double b)
+        {
+            double x = 0f;
+            x = a / b;
+            return x;
+        }
+    }
+}
---
+++ ConsoleCalulator/ConsoleCalulator/Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+
+namespace ConsoleCalulator
+{
+    class Program
+    {
+        /// <summary>
+        /// This is the main driver that runs the calculator.
+        /// </summary>
+        static void Main()
+        {
+            Say.Write("Welcome to the Console Calculator!", true, 20f, ConsoleColor.Green);
+
+            bool UserCont = true;
+            string UserChoice;
+            
+            //Checks to see if User wants to run another calculation.
+            while (UserCont == true)
+            {
+                Calculator Calc = new Calculator();
+                Calc.Calculate();
+
+                Say.Write("Run another Calculation? (Yes/No)", true, 20f, ConsoleColor.Green);
+                UserChoice = Console.ReadLine().ToLower();
+
+                if (UserChoice == "no" || UserChoice == "n")
+                {
+                    UserCont = false;
+                    Say.Write("Thank You", true, 20f, ConsoleColor.Green);
+                }
+            }
+
+        }
+    }
+
+    class Calculator
+    {
+        /// <summary>
+        /// This is the main calculator method for the basic functions
+        /// </summary>
+        public void Calculate()
+        {
+            int num1 = 0;
+            int num2 = 0;
+            char UserOp;
+
+            Say.Write("Please enter the first number", true, 25f, ConsoleColor.Green);
+            Say.Write(">>", false, 20f, ConsoleColor.Green);
+            String input1 = Console.ReadLine();
+
+            //User validation for integer
+            while (!Int32.TryParse(input1, out num1))
+            {
+                Say.Write("Not a valid number. Please try again!", true, 25f, ConsoleColor.Red);
+                Say.Write(">>", false, 20f, ConsoleColor.Green);
+                input1 = Console.ReadLine();
+            }
+
+            Say.Write("Please enter an operator: +, -, *, /, %, ^, s ", true, 25f, ConsoleColor.Green);
+            Say.Write(">>", false, 50f, ConsoleColor.Green);
+            UserOp = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            //User validation for math operators
+            while (!CheckOperation(UserOp))
+            {
+                Say.Write("That is not a valid operator!", true, 25f, ConsoleColor.Red);
+                Say.Write("Please enter an operator: +, -, *, /, %, ^, s", true, 25f, ConsoleColor.Green);
+                Say.Write(">>", false, 50f, ConsoleColor.Green);
+                UserOp = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
+
+            //skip second input prompt if user selects squareroot operator.
+            if (UserOp != 's')
+            {
+                //User validation for integer
+                Say.Write("Please enter the second number", true, 25f, ConsoleColor.Green);
+                Say.Write(">>", false, 50f, ConsoleColor.Green);
+                string input2 = Console.ReadLine();
+
+                while (!Int32.TryParse(input2, out num2))
+                {
+                    Say.Write("Not a valid number. Please try again!", true, 25f, ConsoleColor.Red);
+                    Say.Write(">>", false, 20f, ConsoleColor.Green);
+                    input2 = Console.ReadLine();
+                }
+            }
+
+            //This is prompt for output
+            Say.Write(">>>>", false, 20f, ConsoleColor.Yellow);
+
+            //Calculation functions
+            switch (UserOp)
+            {
+                //case '+': Console.WriteLine(num1 + num2); break;
+                case '+': Console.WriteLine(Addition(num1, num2)); break;
+
+                case '-': Console.WriteLine(Subtraction(num1, num2)); break;
+
+                case '*': Console.WriteLine(Multiplication(num1, num2)); break;
+
+                case '^': Console.WriteLine(PowerOf(num1, num2)); break;
+
+                case '%': Console.WriteLine(Modulo(num1, num2)); break;
+
+                case '/':
+
+                    if (num2 == 0)
+                    {
+                        Say.Write("Error! The denominator cannot be 0", true, 25f, ConsoleColor.Red);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(Division(num1, num2)); break;
+                    }
+
+                case 's': Console.WriteLine(SquareRoot(num1)); break;
+            }
+        }
+
+#region MathFunctions
+        /// <summary>
+        /// Math functions for switch cases.
+        /// </summary>
+        /// <param name="num1">user's first input</param>
+        /// <param name="num2">user's second input</param>
+        /// <returns></returns>
+        public int Addition(int num1, int num2)
+        {
+            int Answer = num1 + num2;
+            return Answer;
+        }
+
+        public int Subtraction (int num1, int num2)
+        {
+            int Answer = num1 - num2;
+            return Answer;
+        }
+
+        public int Multiplication(int num1, int num2)
+        {
+            int Answer = num1 * num2;
+            return Answer;
+        }
+
+        public int Division(int num1, int num2)
+        {
+            int Answer = num1 / num2;
+            return Answer;
+        }
+
+        public int Modulo(int num1, int num2)
+        {
+            int Answer = num1 % num2;
+            return Answer;
+        }
+
+        public double PowerOf(int num1, int num2)
+        {
+            double Answer = Math.Pow(num1, num2);
+            return Answer;
+        }
+
+        public double SquareRoot(int num1)
+        {
+            double Answer = Math.Sqrt(num1);
+            return Answer;
+        }
+#endregion 
+
+        /// <summary>
+        /// This method validates the operator enter by the user
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <returns></returns>
+        public bool CheckOperation(char operation)
+        {
+            switch (operation)
+            {
+                case '*':
+                    return true;
+                    break;
+                case '+':
+                    return true;
+                    break;
+                case '-':
+                    return true;
+                    break;
+                case '/':
+                    return true;
+                    break;
+                case '^':
+                    return true;
+                    break;
+                case '%':
+                    return true;
+                    break;
+                case 's':
+                    return true;
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// This class is for customizable console output.
+    /// </summary>
+    class Say
+    {
+        /// <summary>
+        /// This method is for custom console output with text lag and color
+        /// </summary>
+        /// <param name="toSay">string to output</param>
+        /// <param name="newLine">if a new line is required after text</param>
+        /// <param name="speed">speed the text is displayed</param>
+        /// <param name="color">color of the text</param>
+        public static void Write(string toSay, bool newLine, float speed, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+
+            foreach (char toWrite in toSay)
+            {
+                Console.Write(toWrite);
+                System.Threading.Thread.Sleep((int)speed);
+            }
+            if (newLine == true)
+            {
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+    }
+}
