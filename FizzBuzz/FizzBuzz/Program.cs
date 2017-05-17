8c36369576171668a729d1387dd6445b16336921
w3iiipu <w3iiipu@gmail.com>
2017-05-17 08:07:23 -0400

    added c# fizz buzz no testing

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int UserInput = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 1; i<=UserInput; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+        }
+
+        public static string FizzBuzz(int x)
+        {
+
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                return ">>>FizzBuzz<<<";
+            }
+            else
+            if (x % 5 == 0)
+            {
+                return ">>Buzz<<";
+            }
+            else
+            if (x % 3 == 0)
+            {
+                return ">>Fizz<<";
+            }
+            else
+                return x.ToString();
+        }
+
+
+    }
+}
